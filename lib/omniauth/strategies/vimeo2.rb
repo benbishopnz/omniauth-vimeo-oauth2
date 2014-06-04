5b18daae3f49f2ad371f6f74e45e92a447be04f9
Adam Haris <iamadamharis@gmail.com>
2014-06-04 15:35:50 +0200

    first working version

--- lib/omniauth/strategies/vimeo2.rb
+++ lib/omniauth/strategies/vimeo2.rb
@@ -9,11 +9,26 @@ module OmniAuth
         :site => "https://api.vimeo.com",
         :request_token_url => "/oauth/request_token",
         :authorize_url => "/oauth/authorize",
-        :access_token_path => "/oauth/access_token"
+        :token_url => "/oauth/access_token",
       }
 
-      #TODO
-      #params
+      uid { raw_info['uri'].gsub('/users/', '') }
+
+      # https://developer.vimeo.com/api/endpoints/users#/users/+user_id
+      info do
+        {
+          name: raw_info['name'],
+          uri: raw_info['uri'],
+          link: raw_info['link'],
+          location: raw_info['location'],
+          bio: raw_info['bio'],
+          created_time: raw_info['created_time']
+        }
+      end
+
+      def raw_info
+        @raw_info ||= MultiJson.decode(access_token.get('/me').body)
+      end
     end
   end
 end
